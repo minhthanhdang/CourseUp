46e5f6855f8e60ffcce63beb21d10195e0d13f65
minh-thanh-dang <mtddev2004@gmail.com>
2024-01-06 01:08:47 +1100

    add packages, tailwind customs

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,11 +1,18 @@
-import { useState } from 'react'
-
+import { Routes, Route } from 'react-router-dom';
+import NavLeft from './components/NavLeft/NavLeft';
+import Test from './pages/Test';
+import Login from './pages/Login';
 
 function App() {
 
   return (
-    <>
-    </>
+    <Routes>
+      <Route path="/" element={<NavLeft />}>
+
+      </Route>
+      <Route path="/test" element={<Test />} />
+      <Route path="/login" element={<Login />} />
+    </Routes>
   )
 }
 
--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -4,7 +4,19 @@ export default {
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
-    extend: {},
+    fontFamily: {
+      'author': ['Inter']
+    },
+    extend: {
+      colors: {
+        primary: "#111111",
+        blackBackground: "#222222"
+      },
+      boxShadow: {
+        'input': '0 5px 30px 0 rgba(0,0,0,.1)',
+        'purple':  '0 10px 30px 0 rgba(189,89,212,.5)'
+      }
+    },
   },
   plugins: [],
 }
